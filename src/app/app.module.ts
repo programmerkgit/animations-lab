a8659960367a3ccb322eb0f7544242c33dbe35f9
admin <k.kubo.private.mobile@gmail.com>
2019-06-18 07:49:53 +0900

    animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SlideComponent } from './animations/slide/slide.component';
+import { IndexComponent } from './animations/index/index.component';
+import { RotateComponent } from './animations/rotate/rotate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideComponent,
+    IndexComponent,
+    RotateComponent
   ],
   imports: [
     BrowserModule
